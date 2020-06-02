ce398e78f6a3a6098e22ca589c4e3904749e5787
amorim66 <gabriel1amorim2013@gmail.com>
2020-06-02 00:05:21 -0300

    Menu de navegacao

---
+++ MYEB_MOBILE/MYEB_MOBILE/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MYEB_MOBILE
+{
+    public partial class App : Application
+    {
+        public static MasterDetailPage MasterDetail { get; set; } //navegar a partir de master
+
+        public async static Task NavigateMasterDetail(Page page) // Metodo de passagem das paginas
+        {
+            App.MasterDetail.IsPresented = false; // fechar masterpage durante a navegacao
+            await App.MasterDetail.Detail.Navigation.PushAsync(page);
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MYEB_MOBILE/MYEB_MOBILE/Master.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MYEB_MOBILE
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : ContentPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+            Button1.Clicked += async (sender, e) =>
+            {
+                await App.NavigateMasterDetail(new Pagina1()); // passagem para a pagina teste 1
+            };
+
+
+            Button2.Clicked += async (sender, e) =>
+            {
+                await App.NavigateMasterDetail(new Pagina2()); // passagem para a pagina teste 2
+            };
+        }
+    }
+}
